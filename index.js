b27ef8e8b434da66141b66385e68f134aca43a07
gabhabry <gabriel.habryn@schibsted.pl>
2014-11-27 08:40:50 +0100

    Initial template and express integration

---
+++ index.js
@@ -0,0 +1,28 @@
+// https://www.googleapis.com/auth/calendar
+// https://www.googleapis.com/auth/calendar
+var express = require('express');
+var fs = require('fs'); // this engine requires the fs module
+
+var app = express();
+
+app.engine('html', function (filePath, options, callback) {
+    fs.readFile(filePath, function (err, content) {
+        if (err) throw new Error(err);
+        return callback(null, content.toString());
+    })
+});
+app.set('views', './templates'); // specify the views directory
+app.set('view engine', 'html');
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+var server = app.listen(3000, function () {
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port)
+
+});
